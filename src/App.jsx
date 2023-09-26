23eb3396bc75c0275303e4db2dd41cc182486320
icequeenwand <ytredqueen@gmail.com>
2023-09-26 19:51:13 +0800

    finish header (navbar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import HomePage from "./pages/HomePage.jsx";
 import DestinationPage from "./pages/DestinationPage.jsx";
 import ActivityPage from "./pages/ActivityPage.jsx";
+import Header from "./components/globals/Header.jsx";
+
+// TODO: import data
+import destinations from "./data/destinations.json";
+import activities from "./data/activities.json";
+import categories from "./data/categories.json";
+import navigations from "./data/navigations.json";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path={"/"} element={<HomePage />} />
-        <Route path={"/destinations"} element={<DestinationPage />} />
-        <Route path={"/activities"} element={<ActivityPage />} />
-        <Route path={"*"} element={<h1>Page not found</h1>} />
-      </Routes>
-    </Router>
+    <>
+      <Header navigations={navigations} />
+      <Router>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <HomePage destinations={destinations} categories={categories} />
+            }
+          />
+          <Route
+            path={"/destinations"}
+            element={<DestinationPage destinations={destinations} />}
+          />
+          <Route
+            path={"/activities"}
+            element={<ActivityPage activities={activities} />}
+          />
+          <Route
+            path={"*"}
+            element={
+              <main className="w-full bg-white h-screen flex flex-col justify-center items-center font-bold">
+                <ExclamationTriangleIcon className="w-10 h-10" />
+                <span className="text-2xl text-neutral-700">
+                  Page not found
+                </span>
+              </main>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 };
 export default App;
---
+++ src/components/globals/Header.jsx
@@ -0,0 +1,85 @@
+import {
+  MoonIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+const Header = ({ navigations }) => {
+  const [showMobileOption, setShowMobileOption] = useState(false);
+
+  return (
+    <header className="w-full z-50 fixed flex justify-center items-center">
+      <nav className="w-full bg-white z-40 p-4 flex justify-between items-center">
+        <div className="flex justify-center items-center gap-2">
+          <a
+            href="/"
+            className="text-indigo-400 pr-2 text-base md:text-lg lg:text-xl font-bold"
+          >
+            {import.meta.env.VITE_APP}
+          </a>
+          <div className="border-l px-3">
+            {showMobileOption ? (
+              <XMarkIcon
+                className="w-5 md:hidden"
+                onClick={() => setShowMobileOption(!showMobileOption)}
+              />
+            ) : (
+              <ChevronDownIcon
+                className="w-5 md:hidden"
+                onClick={() => setShowMobileOption(!showMobileOption)}
+              />
+            )}
+
+            <ul className="hidden md:flex gap-2">
+              {navigations.map((item, idx) => {
+                return (
+                  <li key={idx}>
+                    <a
+                      href={item.path}
+                      className="text-sm text-neutral-600 lg:text-base hover:font-semibold hover:text-indigo-400"
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <div className="hidden lg:flex p-1 border rounded-full">
+            <MagnifyingGlassIcon className="w-4 mx-1" />
+            <input
+              type="text"
+              className="active:outline-none px-2 text-sm w-32 focus:outline-none outline-0"
+            />
+          </div>
+          <MagnifyingGlassIcon className="w-5 lg:hidden" />
+          <MoonIcon className="w-5" />
+        </div>
+      </nav>
+
+      <nav
+        className={`absolute rounded-b-3xl z-30 w-full flex justify-center items-center ${
+          showMobileOption ? "top-14" : "top-[-100px]"
+        }  md:hidden bg-indigo-400`}
+      >
+        <ul className="flex flex-col justify-center items-center p-8 gap-2">
+          {navigations.map((item, idx) => {
+            return (
+              <li key={idx}>
+                <a className="text-sm text-white" href={item.path}>
+                  {item.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
