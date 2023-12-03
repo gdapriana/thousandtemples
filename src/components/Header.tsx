54257fb1397bfd4fa02e04ced9ecb2705f3813aa
icequeenwand <ytredqueen@gmail.com>
2023-12-03 17:28:44 +0800

    navbar v2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function Header({ navigations }: { navigations: any }) {
 	return (
@@ -7,7 +10,7 @@ export default function Header({ navigations }: { navigations: any }) {
 			<nav className='w-full flex items-center max-w-7xl justify-between'>
 				<Navbrand />
 				<Navmenu navigations={navigations} />
-				<Navtools />
+				<Navtools navigations={navigations} />
 			</nav>
 		</header>
 	);
@@ -38,12 +41,12 @@ function Navmenu({ navigations }: { navigations: any }) {
 										xmlns='http://www.w3.org/2000/svg'
 										fill='none'
 										viewBox='0 0 24 24'
-										stroke-width='1.5'
+										strokeWidth='1.5'
 										stroke='currentColor'
 										className='w-4 h-4'>
 										<path
-											stroke-linecap='round'
-											stroke-linejoin='round'
+											strokeLinecap='round'
+											strokeLinejoin='round'
 											d={item.icon}
 										/>
 									</svg>
@@ -58,7 +61,8 @@ function Navmenu({ navigations }: { navigations: any }) {
 	);
 }
 
-function Navtools() {
+function Navtools({ navigations }: { navigations: any }) {
+	const [showMenu, setShowMenu] = useState(false);
 	return (
 		<div className='flex justify-center items-center md:gap-4'>
 			<label className='swap swap-rotate text-primary'>
@@ -81,9 +85,14 @@ function Navtools() {
 					<path d='M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z' />
 				</svg>
 			</label>
-			<label className='btn btn-circle border-none md:hidden swap swap-rotate text-primary bg-white'>
+			<label className='btn btn-circle border-none md:hidden hover:bg-white swap swap-rotate text-primary bg-white'>
 				{/* this hidden checkbox controls the state */}
-				<input type='checkbox' />
+				<input
+					onClick={() => {
+						setShowMenu(!showMenu);
+					}}
+					type='checkbox'
+				/>
 
 				{/* hamburger icon */}
 				<svg
@@ -109,6 +118,58 @@ function Navtools() {
 				<ArrowRightOnRectangleIcon className='w-5' />
 				Login
 			</button>
+
+			<FloatMenu
+				showMenu={showMenu}
+				setShowMenu={setShowMenu}
+				navigations={navigations}
+			/>
+		</div>
+	);
+}
+
+function FloatMenu({
+	showMenu,
+	setShowMenu,
+	navigations,
+}: {
+	showMenu: any;
+	setShowMenu: any;
+	navigations: any;
+}) {
+	return (
+		<div
+			className={`absolute md:hidden ease-in-out duration-1000 top-24 ${
+				showMenu ? "right-4" : "right-[-300px] opacity-0"
+			}`}>
+			<ul className='menu bg-white w-56 rounded-box'>
+				{navigations.map(
+					(item: { path: string; name: string; icon: string }, i: number) => {
+						return (
+							<li key={i}>
+								<Link
+									className='text-neutral-400 hover:text-primary duration-300 ease-in-out text-xs sm:text-sm font-bold'
+									href={item.path}>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										fill='none'
+										viewBox='0 0 24 24'
+										strokeWidth='1.5'
+										stroke='currentColor'
+										className='w-4 h-4'>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											d={item.icon}
+										/>
+									</svg>
+									{item.name}
+								</Link>
+							</li>
+						);
+					}
+				)}
+			</ul>
 		</div>
 	);
 }
